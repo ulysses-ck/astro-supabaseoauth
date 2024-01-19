02fd0733571f6b89674e1b37b408f92621a824d6
ulysses-ck <ulisesapaza.280@gmail.com>
2024-01-19 14:20:09 -0300

    start project

---
+++ src/pages/api/auth/signin.ts
@@ -0,0 +1,53 @@
+import type { APIRoute } from "astro";
+import type { Provider } from "@supabase/supabase-js";
+import supabase from "../../../lib/supabase";
+
+export const POST: APIRoute = async ({ request, cookies, redirect }) => {
+	const formData = await request.formData();
+	const email = formData.get("email")?.toString();
+	const password = formData.get("password")?.toString();
+	const provider = formData.get("provider")?.toString();
+
+	if (provider) {
+		const { data, error } = await supabase.auth.signInWithOAuth({
+			provider: provider as Provider,
+			options: {
+				redirectTo: "http://localhost:4321/api/auth/callback",
+			},
+		});
+
+		if (error) {
+			return new Response(error.message, { status: 500 });
+		}
+
+		console.log(data);
+		return redirect(data.url);
+	}
+
+	if (!email || !password) {
+		return new Response("Email and password are required", { status: 400 });
+	}
+
+	const { data, error } = await supabase.auth.signInWithPassword({
+		email,
+		password,
+	});
+
+	if (error) {
+		return new Response(error.message, { status: 500 });
+	}
+
+	const { access_token, refresh_token } = data.session;
+	cookies.set("sb-access-token", access_token, {
+		sameSite: "strict",
+		path: "/",
+		secure: true,
+	});
+	cookies.set("sb-refresh-token", refresh_token, {
+		sameSite: "strict",
+		path: "/",
+		secure: true,
+	});
+
+	return redirect("/dashboard");
+};
